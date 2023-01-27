9cbe04ac7727cf53dcd29831a4e58eed209647cf
hoeco <hett.pie@gmail.com>
2023-01-26 20:35:11 -0600

    update 2.13.1

---
+++ part6/counter-redux/src/reducers/noteReducer.js
@@ -0,0 +1,9 @@
+const noteReducer = (state = [], action) => {
+    switch (action.type) {
+      case 'NEW_NOTE':
+        return state.concat(action.data)
+      default:
+        return state
+    }
+  }
+  
